48bad0ede430dbc13ceeb42e12ff7c014ddd5f0a
Bilal Abu Saad <bilalabusaad@gmail.com>
2024-03-09 22:35:32 -0500

    read some messages -- code is still a mess but getting closer to replying to a message

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,85 @@
 
 use std::io::{self};
 
-use serde_json::Value;
-use anyhow::{Result};
+use serde_json::{Value};
+use anyhow::Result;
+
+
+
+// Maelstrom RPC msg
+#[derive(Debug)]
+struct Message {
+    // ID of node sending this.
+    src: String, 
+    // ID of node this message is for.
+    dest: String,
+    // Body of the message.
+    // {
+    //   "type":        (mandatory) A string identifying the type of message this is
+    //   "msg_id":      (optional)  A unique integer identifier
+    //    "in_reply_to": (optional)  For req/response, the msg_id of the request
+    //  }
+    body: Value,
+}
+
+
+enum Type {
+    // Unknown, string is the type
+    Unknown(String),
+    // Init RPC
+    Init,
+}
+
+
+impl Message {
+    fn new(v: Value) -> Result<Self> {
+        let src: String = v.get("src").expect("todo fix src in msg").as_str().expect("sheet").to_string();
+        let dest: String = v.get("dest").expect("todo fix dest in msg").as_str().expect("sheeet dest").to_string();
+        let body: Value = v.get("body").expect("shit todo no body in msg").clone();
+
+        Ok(Self{
+            src,
+            dest,
+            body
+        })
+    }
+
+    fn msg_type(&self) -> Type {
+        match self.body.get("type") {
+            Some(v) => {
+                if v.as_str().is_some_and(|x| x == "init") {
+                    return Type::Init
+                }
+                return Type::Unknown(v.as_str().unwrap_or("unknown type").to_string())
+            }
+            None => {panic!("Message with no type in body...");}
+        }
+    }
+}
 
 fn main() -> Result<()> {
+
+
+    eprintln!("Node starting...");
+
     let mut buffer = String::new();
-
     let stdin = io::stdin();
-
-    stdin.read_line(&mut buffer)?;
 
     while stdin.read_line(&mut buffer).is_ok() {
         match serde_json::from_str::<Value>(&buffer) {
             Ok(v) => {
-                println!("{}", v);
+                eprintln!("Recieved Json: {}", v);
+                let msg = Message::new(v)?;
+
+                match msg.msg_type() {
+                    Type::Init => {
+                        eprintln!("initialized node with id:  {:?}", msg.body.get("node_id"));
+
+                    }
+                    Type::Unknown(s) => {
+                        eprintln!("Recieved unknown msg type:  {:?}", msg);
+                    }
+                }
             }
             Err(e) => { eprintln!("Failed to parse json {}", e);}
         }
